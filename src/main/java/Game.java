1c8266dd672e585011b92405473f99b13f5d5532
Luis <luis.santanaholmes@gmail.com>
2021-10-29 23:56:00 +0200

    refactored code into Game class from Main class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,5 @@
+import tetris.gui.ActionEvent;
+import tetris.gui.ActionHandler;
 import tetris.gui.Block;
 import tetris.gui.GUI;
 
@@ -6,12 +8,70 @@ public class Game {
     private static final GUI gui = new GUI(10, 20);
     private static Block block;
 
-
     public Game() {
+        start();
     }
 
-    public void start() {
+
+    //********* Handles all the movements of the blocks ************************
+    private static final ActionHandler actionHandler = new ActionHandler() {
+        @Override
+        public void moveDown() {
+            block.y--;
+            gui.clear();
+            gui.drawBlock(block);
+        }
+
+        @Override
+        public void moveLeft() {
+            block.x--;
+            gui.clear();
+            gui.drawBlock(block);
+        }
+
+        @Override
+        public void moveRight() {
+            block.x++;
+            gui.clear();
+            gui.drawBlock(block);
+        }
+
+        @Override
+        public void rotateLeft() {
+
+        }
+
+        @Override
+        public void rotateRight() {
+
+        }
+
+        @Override
+        public void drop() {
+
+        }
+    };
+//***************************************************
+
+
+    public static void start() {
         System.out.println("This method will start the game!");
+
+        /*
+         * Block setup
+         * left to right position
+         * i: 0 - 9
+         * bottom to top position
+         * i1:0 - 19
+         * colors of blocks
+         * i2: red = 1, yellow = 2, pink = 3, blue = 4, turquoise = 5, green = 6, grey = 7,
+         * */
+        createBlock(9, 19, 3);
+
+        while (true) {
+            handleEvent();
+        }
+
     }
 
     public static void createBlock(int i, int i1, int i2) {
@@ -22,10 +82,17 @@ public class Game {
 
     public static void handleEvent() {
         System.out.println("call method \"handleEvent\"");
+
+        ActionEvent event = gui.waitEvent();
+        switch (event) {
+            case MOVE_LEFT -> actionHandler.moveLeft();
+            case MOVE_RIGHT -> actionHandler.moveRight();
+            case MOVE_DOWN -> actionHandler.moveDown();
+        }
     }
 
-    public void updateGUI() {
-        System.out.println("This method will update the GUI!");
+    public static void updateGUI() {
+        System.out.println("call method \"updateGUI\"");
     }
 
 }
--- src/main/java/Tetris.java
+++ src/main/java/Tetris.java
@@ -1,18 +1,7 @@
 public class Tetris {
 
     public static void main(String[] args) {
-
-        // create Block method
-        Game.createBlock(5,5,1);
-
-
-//        while(true){
-//
-//            ActionEvent event = gui.waitEvent();
-//            block.x++;
-//            gui.clear();
-//            gui.drawBlock(block);
-//        }
+        Game.start();
 
     }
 }
